f2723667491f47f427bb7443cdfaa74be8df7bef
Marwan Abu-Khalil <seminar@marwan.biz>
2025-04-26 16:07:56 +0200

    cpu intensive call in 4.2.a eingebaut

--- src/seminar_reactor/exercises/rc_4_parallel/rc_4_2_a_publishon_speedup/solution/PublishOnSpeedup_Solution.java
+++ src/seminar_reactor/exercises/rc_4_parallel/rc_4_2_a_publishon_speedup/solution/PublishOnSpeedup_Solution.java
@@ -5,9 +5,14 @@ import reactor.core.scheduler.Schedulers;
 
 /*
  * Zeige, dass sich ein Stream durch den Einsatz unterschiedlicher 
- * Threads für die verschiednen Stream-Stufen beschleunigen laesst. 
+ * Threads für die verschiedenen Stream-Stufen beschleunigen laesst. 
  * 
  * Hinweis: publishOn() an geeigneter Stelle in den vorhanden Stream einbringen
+ * 
+ * Bemerkung: Unter welchen Bedingungen laufen die beiden Stufen in verschiedenen Threads? 
+ * Es haengt davon ab, ob die Stufen CPU bound oder IO bound sind. Versuche dies durch die 
+ * Aufrufe sleep() oder cpuIntensiveCall() herzustellen.
+ * 
  */
 public class PublishOnSpeedup_Solution {
 
@@ -32,9 +37,10 @@ public class PublishOnSpeedup_Solution {
 		source
 				// Laeuft im Main-Thread
 				.map(i -> {
-					System.out.println("map: " + i + " " + Thread.currentThread().getName());
+					System.out.println("map: " + i + "  " + Thread.currentThread());
 					// Dieser Aufruf macht die Map-Stufe langsam
 					sleep(100);
+					//cpuIntensiveCall();
 					return i;
 				})
 				
@@ -44,10 +50,10 @@ public class PublishOnSpeedup_Solution {
 
 				// Laeuft im Pool-Thread
 				.subscribe(i -> {
-					System.out.println("Subscriber: " + i + " " + Thread.currentThread().getName());
+					System.out.println("Subscriber: " + i + "  " + Thread.currentThread());
 					// Dieser Aufruf macht den Subscriber langsam
 					sleep(100);
-
+					//cpuIntensiveCall();
 					// Zeitmessung
 					if (i == 50) {
 						System.out.println("\n Stream Completed: Runtime: "
@@ -68,5 +74,26 @@ public class PublishOnSpeedup_Solution {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/*
+	 * Dieser Aufruf laueft ca. 100 Millisekunden auf (meiner) CPU 
+	 */
+	static void cpuIntensiveCall() {
+		long start = System.currentTimeMillis();
+		
+		long dummy = 2;
+		for(long counter = 0; counter <= Integer.MAX_VALUE / 5; counter ++) {
+				dummy = (dummy + dummy);
+				if (dummy > Integer.MAX_VALUE) {
+					dummy = dummy % Integer.MAX_VALUE;
+					
+				}
+				
+		}
+		
+		long runtime = System.currentTimeMillis() - start;
+		
+		//System.out.println("cpuIntensiveCall: runtime " + runtime + " Thread: " + Thread.currentThread());
 	}
 }
